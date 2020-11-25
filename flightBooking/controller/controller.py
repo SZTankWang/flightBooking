aeb9b35645744782afaf235106f7278cc2140803
SZTankWang <2938599137@qq.com>
2020-11-25 12:46:13 +0800

    11.25 更新base html

--- flightBooking/controller/controller.py
+++ flightBooking/controller/controller.py
@@ -2,10 +2,17 @@ from flightBooking import app
 from flask import render_template
 
 
+
+
+
 @app.route('/eFlight')
 def Hello():
     print('Hi')
     return render_template('customerHome.html')
+
+@app.route('/eFlight/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/eFlight/viewFlight')
 def viewFlight():
