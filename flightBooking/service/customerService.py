a0579e06a2fcc606c48c45faae746de91340072a
Ren Sheng <49581367+Madjazz4@users.noreply.github.com>
2020-12-06 00:50:41 +0800

    push

--- flightBooking/service/customerService.py
+++ flightBooking/service/customerService.py
@@ -3,7 +3,8 @@ from sqlalchemy import text
 from flightBooking import db
 
 def checkLogin(type,userName,password):
+    db.session.execute(text("set @msg = '0'"))
+    db.session.execute(text("set @code = 0"))
     db.session.execute(text("CALL eflight.login_check(:p1,:p2,:p3,@msg,@code)"),{"p1":type,"p2":userName,"p3":password})
-    result = db.session.execute(text("select @msg")).fetchone()
-    code = db.session.execute(text("select @code")).fetchone()
-    return result[0],code[0]
+    result = db.session.execute(text("select @msg,@code")).fetchone()
+    return result
