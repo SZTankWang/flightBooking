b673da3f2af675dde936bd07f58618cfca149e54
Ren Sheng <49581367+Madjazz4@users.noreply.github.com>
2020-12-05 23:05:56 +0800

    push

--- flightBooking/controller/controller.py
+++ flightBooking/controller/controller.py
@@ -1,6 +1,15 @@
 from flightBooking import app
-from flask import render_template
-
+import collections
+from datetime import timedelta
+from itertools import count
+from time import strftime
+from flask import Flask, render_template, redirect, url_for, request, json, jsonify, session, flash, make_response
+from flask.signals import appcontext_tearing_down
+from flask_login.utils import logout_user
+from flask_login import login_user, logout_user, current_user, login_required
+from numpy.core.arrayprint import TimedeltaFormat
+from numpy.lib.function_base import select
+from sqlalchemy.util.langhelpers import methods_equivalent
 
 
 
@@ -10,15 +19,24 @@ def Hello():
     print('Hi')
     return render_template('customerHome.html')
 
-@app.route('/eFlight/login/<type>')
-def login(type):
-    return render_template('login.html',type=type)
+@app.route('/eFlight/gologin')
+def gologin():
+    return render_template('login.html')
+
+@app.route('/eFlight/dologin/<type>')
+def dologin(type):
+    id = request.form['id']
+    password = request.form['password']
+    db.session.execute(text("set @msg = '0'"))
+    db.session.execute(text("CALL eflight.login_check(:p1,:p2,:p3,@msg)"),{"p1":type,"p2":id,"p3":password})
+    result = db.session.execute(text("select @msg")).fetchone()
+    return result
 
 @app.route('/eFlight/register/<type>')
 def register(type):
         if type == 'agent':
             ''' 查询系统记录中的airline name, 放入 airline_names 变量中
-            ''' 
+            '''
         return render_template('register.html',type=type, airline_name=None)
 
 @app.route('/eFlight/viewFlight')
