616121273791dcbd0c195d07c6360b7e6cfe5a5e
Madjazz4 <49581367+Madjazz4@users.noreply.github.com>
2020-12-09 22:23:13 +0800

    Update controller.py

--- flightBooking/controller/controller.py
+++ flightBooking/controller/controller.py
@@ -257,9 +257,9 @@ def viewBookingAgent():
     resultproxy1 = db.session.execute(text("CALL eflight.get_top_commission_agent(:p1,365)"),{"p1":staffID})
     year_commission_result = [{column: float(value) if type(value) == decimal.Decimal else value for column, value in rowproxy.items()} for rowproxy in resultproxy1][0]
     resultproxy2 = db.session.execute(text("CALL eflight.get_top_ticket_agent(:p1,30)"),{"p1":staffID})
-    month_ticket_result = [{column: float(value) if type(value) == decimal.Decimal else value for column, value in rowproxy.items()} for rowproxy in resultproxy1][0]
+    month_ticket_result = [{column: float(value) if type(value) == decimal.Decimal else value for column, value in rowproxy.items()} for rowproxy in resultproxy2][0]
     resultproxy3 = db.session.execute(text("CALL eflight.get_top_ticket_agent(:p1,365)"),{"p1":staffID})
-    year_ticket_result = [{column: float(value) if type(value) == decimal.Decimal else value for column, value in rowproxy.items()} for rowproxy in resultproxy1][0]
+    year_ticket_result = [{column: float(value) if type(value) == decimal.Decimal else value for column, value in rowproxy.items()} for rowproxy in resultproxy3][0]
     return jsonify(year_commission_result=year_commission_result,month_ticket_result=month_ticket_result,year_ticket_result=year_ticket_result)
 
 @app.route('/eFlight/trackSpending')
